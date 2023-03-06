294483500e9e6245452529718d1a7805a345c7ad
Matheus7k <06matheus.oliveira@gmail.com>
2023-03-05 23:53:28 -0300

    Add project files.

---
+++ tabuada/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    static void Tabuada(int numero)
+    {
+        if (numero >= 1 || numero <= 9)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"{numero} x {i} = {numero * i}");
+            }
+        }
+    }
+    private static void Main(string[] args)
+    {
+        int numero;
+
+        Console.Write("Informe um numero para a tabuada: ");
+        numero = int.Parse(Console.ReadLine());
+
+        Tabuada(numero);
+    }
+}
